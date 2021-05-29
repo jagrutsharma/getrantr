8e72d7baf8f27de23255d61a32b469e63d32c5a2
jagsharm <jagsharm@adobe.com>
2021-05-29 14:51:14 -0700

    challenge-5-done

--- src/public/scripts/adListView.js
+++ src/public/scripts/adListView.js
@@ -1,59 +1,64 @@
 var AdView = Backbone.View.extend({
-
-  template: function(ad) {
+  template: function (ad) {
     return `<div class="ad clearfix">
               <a class="ad-link" href="${ad.targetURL}" target="_blank" data-advertiserId="${ad.advertiserId}">
                 <img src="${ad.imageURL}" />
               </a>
-            </div>`;
+            </div>`
   },
 
-  render: function() {
-    this.$el.html(this.template(this.model.toJSON()));
-    return this;
-  }
-
-});
+  render: function () {
+    this.$el.html(this.template(this.model.toJSON()))
+    return this
+  },
+})
 
 var AdListView = Backbone.View.extend({
-
   children: [],
 
-  initialize: function() {
-    this.collection.on('reset', this.render, this);
-    this.collection.on('add', this.renderAd, this);
-    this.$el.on('click', function(evt) {
-      var clicked = evt.target.parentElement;
+  initialize: function () {
+    this.collection.on('reset', this.render, this)
+    this.collection.on('add', this.renderAd, this)
+    this.$el.on('click', function (evt) {
+      var clicked = evt.target.parentElement
       if (clicked.matches('.ad-link')) {
-        recordClickAnalytics(clicked.getAttribute('data-advertiserId'));
+        recordClickAnalytics(clicked.getAttribute('data-advertiserId'))
       }
-    });
+    })
     // start ad rotation
-    this.collection.fetch();
-    setInterval(function() {
-      this.collection.fetch({ reset: true });
-    }.bind(this), 10000);
+    this.collection.fetch()
+    setInterval(
+      function () {
+        this.collection.fetch({ reset: true })
+      }.bind(this),
+      10000
+    )
   },
 
-  render: function() {
-    this.el.innerHTML = '';
+  render: function () {
+    this.el.innerHTML = ''
+    //clearning out earlier allocation
+    //otherwise, we will have memory leak
+    this.children.forEach((child) => {
+      child.remove()
+    })
+    this.children.length = 0
+
     this.collection.each((model) => {
-      this.renderAd(model);
-    });
-    return this;
+      this.renderAd(model)
+    })
+    return this
   },
 
-  renderAd: function(ad) {
+  renderAd: function (ad) {
     var view = new AdView({
-      model: ad
-    });
-    this.$el.prepend(view.render().$el);
-    this.children.push(view);
-  }
-
-});
-
+      model: ad,
+    })
+    this.$el.prepend(view.render().$el)
+    this.children.push(view)
+  },
+})
 
 function recordClickAnalytics() {
-  console.log('someone clicked on an ad!!');
+  console.log('someone clicked on an ad!!')
 }
