0737a206a65d745b3d2f186a6e9cd752c39f102c
jagsharm <jagsharm@adobe.com>
2021-05-29 17:00:25 -0700

    challenge-8a-done

--- src/public/scripts/addRantView.js
+++ src/public/scripts/addRantView.js
@@ -1,34 +1,36 @@
 var AddRantView = Backbone.View.extend({
-
-  initialize: function() {
-    this.$('textarea').on('keyup', this.onChange.bind(this));
-    this.$('form').on('submit', this.onSubmit.bind(this));
-    var text = localStorage.getItem('next-rant');
+  initialize: function () {
+    this.$('textarea').on('keyup', this.onChange.bind(this))
+    this.$('form').on('submit', this.onSubmit.bind(this))
+    var text = localStorage.getItem('next-rant')
     if (text) {
-      this.$('textarea').val(text);
+      this.$('textarea').val(text)
     }
   },
 
-  onChange: function(evt) {
-    var text = (evt.target || {}).value;
+  onChange: function (evt) {
+    var text = (evt.target || {}).value
     if (typeof text === 'string') {
-      localStorage.setItem('next-rant', text);
+      localStorage.setItem('next-rant', text)
     }
   },
 
-  onSubmit: function(evt) {
-    evt.preventDefault();
-    var form = evt.target;
+  onSubmit: function (evt) {
+    evt.preventDefault()
+    var form = evt.target
     var rant = {
-      text: form.rant.value
-    };
-
-    analytics.trackConversion();
-    if (rant.text.length > 0) {
-      this.collection.create(rant, { wait: true });
+      text: form.rant.value,
     }
 
-    form.rant.value = '';
-  }
+    //guard against the rare case where analytics.js failed to load
+    //We have to check window.analytics since it will be anchored to window.
+    if (window.analytics) {
+      analytics.trackConversion()
+    }
+    if (rant.text.length > 0) {
+      this.collection.create(rant, { wait: true })
+    }
 
-});
+    form.rant.value = ''
+  },
+})
